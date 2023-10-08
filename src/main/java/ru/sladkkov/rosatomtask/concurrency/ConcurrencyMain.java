01b99f327803668c38c6e3253ec13a513d17a2e3
sladkkov <sladkkov@yandex.ru>
2023-10-08 21:42:57 +0400

    Add concurrency practice

---
+++ src/main/java/ru/sladkkov/rosatomtask/concurrency/ConcurrencyMain.java
@@ -0,0 +1,29 @@
+package ru.sladkkov.rosatomtask.concurrency;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.*;
+
+public class ConcurrencyMain {
+    public static void main(String[] args) throws InterruptedException {
+        createExecutorService();
+        while (true) {
+            System.out.println("Работает основная программа");
+            Thread.sleep(1000);
+        }
+    }
+
+    private static void createExecutorService() {
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(new Runnable() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                System.out.println("Асинхронный привет!");
+                Thread.sleep(5000);
+                System.out.println("Асинхронный пока!");
+
+            }
+        }, 0, 10, TimeUnit.SECONDS);
+    }
+}
